d032e8aa620331e4de402aeb3f298da00b9e651d
Márcio Jales <marcio.dojcosta@gmail.com>
2017-02-02 16:18:02 -0300

    Adding Files

--- sprof/etc/sprof_instr.sh
+++ sprof/etc/sprof_instr.sh
@@ -2,9 +2,22 @@
 #
 # Script file to execute the automatic instrumentation of source code
 #
+
+# Copyright (C) 2017 Márcio Jales
 #
-#set -x
-#trap read debug
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 BLUE='\033[0;34m'
 YELLOW='\033[0;33m'
